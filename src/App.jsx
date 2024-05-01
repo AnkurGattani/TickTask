7e24880e6b174f16591e1fabffec7eda407ac576
Ankur Gattani <ankurgattani02@gmail.com>
2024-05-01 08:56:21 +0530

    Add TodoForm and TodoItem components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './context';
+import {TodoForm, TodoItem} from './components'
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -41,10 +42,19 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
+            {/* Todo form */}
+            < TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            { todos.map((todoItem) => { 
+                return (
+                  <div key= {todoItem.id} className='w-full'> 
+                    <TodoItem todo={todoItem}/> 
+                  </div>
+                )}
+              )
+            }
           </div>
         </div>
       </div>
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { useTodo } from '../context';
+
+function TodoForm() {
+	const [todo, setTodo] = useState("");
+
+	const {addTodo} = useTodo();
+
+	const addToTodoList = (e) => {
+		e.preventDefault();
+
+		if(!todo) 	return;
+
+		// addTodo({id: Date.now(), todo: todo, completed: false})
+		addTodo({ todo, completed: false }); // same as above one. see the addTodo function in App.jsx where we require the new todo in the form of an object
+		setTodo(""); // to set the input field again to empty for new todo to be added after adding the current one.
+
+	}
+
+	return (
+		<form onSubmit={addToTodoList} className="flex">
+			<input
+				type="text"
+				placeholder="Write Todo..."
+				className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
+			/>
+			<button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+				Add
+			</button>
+		</form>
+	);
+}
+
+export default TodoForm;
